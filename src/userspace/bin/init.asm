2a9867b367aafd2d7d8f9f2dcda8a27425718843
Drew DeVault <sir@cmpwn.com>
2012-11-14 14:59:59 -0700

    Hold down T during boot up to launch terminal directly

--- src/userspace/bin/init.asm
+++ src/userspace/bin/init.asm
@@ -1,5 +1,6 @@
 #include "kernel.inc"
 #include "macros.inc"
+#include "keys.inc"
 ; Header
     .db 0  ; TODO: Thread flags
     .db 10 ; Stack size
@@ -24,12 +25,26 @@ _:  pop ix
     call memSeekToStart
     dec ix \ dec ix \ dec ix
     ld (ix), $FE
-
-launchCastle:
+    
     kld de, castlePath
     ld (ix + 6), e
     ld (ix + 7), d
+    
+    call getKey
+    cp kT
+    jr z, launchTerminal
+
+launchCastle:
     call launchProgram
+    ret
+    
+launchTerminal:
+    kld de, terminalPath
+    di
+    call launchProgram
+    kld hl, returnToCastle
+    call setReturnPoint
+    ei
     ret
 
 castlePath:
