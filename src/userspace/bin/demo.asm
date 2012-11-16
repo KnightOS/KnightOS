fb65b292ad25a55f5ae31c0ccf738dae9c55a6f3
Drew DeVault <sir@cmpwn.com>
2012-11-15 22:26:28 -0700

    Removed test code in /bin/demo

--- src/userspace/bin/demo.asm
+++ src/userspace/bin/demo.asm
@@ -1,9 +1,6 @@
 .nolist
 #include "stdio.inc"
 #include "macros.inc"
-#include "kernel.inc"
-#include "keys.inc"
-#include "applib.inc"
 .nolist
 ; Header
     .db 0, 20
@@ -13,18 +10,7 @@ start:
     kld hl, message
     ;stdio(printLine)
     rst $10 \ .db stdioId \ call printLine
-    
-    kld de, applibPath
-    call loadLibrary
-    
-    call getKeypadLock
-_:  rst $10 \ .db applibId \ call appGetKey
-    cp kMode
-    jr nz, -_
     ret
     
 message:
     .db "Hello, world!", 0
-    
-applibPath:
-    .db "/lib/applib", 0
