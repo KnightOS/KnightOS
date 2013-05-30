44e2b015dd3a8ffba13044aa2c5e140c60482b90
George Hilliard <thirtythreeforty@lavabit.com>
2013-05-29 22:22:09 -0500

    Replace hackish return code
    Replaced with code that respects a thread's assigned return address.

--- packages/applib/applib.asm
+++ packages/applib/applib.asm
@@ -430,9 +430,20 @@ showErrorAndQuit:
     push af
         or a
         jr z, showError_exitEarly
+        ; Not returning; we can clobber registers at will!
         icall(showError)
-        ; ENORMOUS HACK
-        jp userMemory + 5
+        call getCurrentThreadID
+        call getThreadEntry
+        inc hl \ inc hl \ inc hl
+        ld c, (hl) \ inc hl \ ld b, (hl)
+        push bc \ pop ix
+        call memSeekToStart
+        dec ix \ dec ix
+        ld c, (ix) \ ld b, (ix + 1)
+        add ix, bc
+        ld l, (ix)
+        ld h, (ix + 1)
+        jp (hl)
  
 #include "errors.asm"
 #include "characters.asm"
